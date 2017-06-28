4527f8333504e2c06113d45e341d0e30d3ea75f0
Jonathan Lara <j.lara@signot.com>
2017-06-27 19:09:20 -0500

    nuevo formato de buffer

--- src/images.js
+++ src/images.js
@@ -6,8 +6,10 @@
 import fs from 'fs';
 
 const overWriteImage = (oldImg: string, newImg: string) => {
-    if (newImg) {
-        let decodedImage = new Buffer(newImg.base64, 'base64').toString('binary');
+    if (newImg && newImg.id && newImg.base64) {
+        console.log('newImg.base64 ', newImg.base64)
+        let decodedImage = new Buffer(newImg.base64, 'base64');
+        console.log('decodedImage ', decodedImage)
         fs.writeFile(oldImg, decodedImage, function (err) { });
         /**fs.readFile(newImg.path, function (err, resImgNew) {
             fs.writeFile(oldImg, resImgNew, function (err) { });
