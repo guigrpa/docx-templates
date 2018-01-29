0975eafb6d1e4419c85a7b5495ca0d6b3bc8f95b
vdechef <v.dechef@gmail.com>
2018-01-29 21:56:33 +0100

    Add jszip compression to reduce output size (#13)
    * Add compression when generating file with JSZip
    
    Signed-off-by: Vincent de Chefdebien <v.dechef@gmail.com>
    
    * Add unit test to check zip generation
    
    Signed-off-by: Vincent de Chefdebien <v.dechef@gmail.com>

--- examples/swapi/swapi-node.js
+++ examples/swapi/swapi-node.js
@@ -3,6 +3,7 @@ const createReport = require('../../lib/indexNode');
 
 createReport({
   template: process.argv[2],
+  output: process.argv.length > 3 ? process.argv[3] : null,
   data: query =>
     fetch('http://swapi.apis.guru', {
       method: 'POST',
