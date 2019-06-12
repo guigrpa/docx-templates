cd2b80f6f04118329445572ca94923435ccffbd9
xiangnanscu <280145668@qq.com>
2019-06-12 15:44:29 +0800

    multiple env support

--- packages/docx-templates/src/indexNode.js
+++ packages/docx-templates/src/indexNode.js
@@ -1,4 +1,13 @@
-const createReport = require('./mainNode').default;
-
-module.exports = createReport;
-module.exports.default = createReport;
+(function (global, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined'){
+    module.exports = factory()
+    module.exports.default = factory()
+  } else if(typeof define === 'function' && define.amd) {
+    define(factory)
+  } else {
+    global = global || self
+    global.createReport = factory();
+  }
+}(this, function () {
+  return require('./mainNode').default;
+}))
