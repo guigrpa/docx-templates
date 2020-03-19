f70cce62ebf9725a376465d441ab3ffb041796ee
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-03-19 10:34:01 +0100

    removed dependencies that are only relevant when debugging

--- packages/docx-templates/src/debug.ts
+++ packages/docx-templates/src/debug.ts
@@ -1,9 +1,1 @@
-// Cannot be covered by Flow! The purpose of this file is to isolate
-// an optional dependency, only used in development (so that Flow
-// doesn't throw errors when processing users' projects)
-import { mainStory, chalk, addListener } from 'storyboard';
-import consoleListener from 'storyboard-listener-console';
-
-addListener(consoleListener);
-
-export { mainStory, chalk };
+export default { info: console.log, debug: console.log };
