3d385bde53435b6f6d98e99eac396d7925e728bf
Sebastian Krüger <2pi_r2@gmx.de>
2020-04-23 12:05:49 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -33,8 +33,8 @@ const DEBUG = process.env.DEBUG_DOCX_TEMPLATES;
 // ==========================================
 /**
  * Create Report from docx template
- * 
- * example: 
+ *
+ * example:
  * ```js
  * createReport({
  *   template,
@@ -52,11 +52,61 @@ const DEBUG = process.env.DEBUG_DOCX_TEMPLATES;
  *   noSandbox: false,
  * });
  * ```
- * 
+ *
  * @param options Options for Report
  */
 async function createReport(options: UserOptions): Promise<Uint8Array>;
+/**
+ * Create Report from docx template (get result before finished)
+ *
+ * example:
+ * ```js
+ * createReport({
+ *   template,
+ *   data: query => graphqlServer.execute(query),
+ *   additionalJsContext: {
+ *     // all of these will be available to JS snippets in your template commands
+ *     foo: 'bar',
+ *     qrCode: async url => {
+ *       // do stuff
+ *     },
+ *   },
+ *   cmdDelimiter: '+++',
+ *   literalXmlDelimiter: '||',
+ *   processLineBreaks: true,
+ *   noSandbox: false,
+ * });
+ * ```
+ *
+ * @param options Options for Report
+ * @param _probe 'JS'
+ */
 async function createReport(options: UserOptions, _probe: 'JS'): Promise<Node>;
+/**
+ * Create Report from docx template (get result before finished)
+ *
+ * example:
+ * ```js
+ * createReport({
+ *   template,
+ *   data: query => graphqlServer.execute(query),
+ *   additionalJsContext: {
+ *     // all of these will be available to JS snippets in your template commands
+ *     foo: 'bar',
+ *     qrCode: async url => {
+ *       // do stuff
+ *     },
+ *   },
+ *   cmdDelimiter: '+++',
+ *   literalXmlDelimiter: '||',
+ *   processLineBreaks: true,
+ *   noSandbox: false,
+ * });
+ * ```
+ *
+ * @param options Options for Report
+ * @param _probe 'XML'
+ */
 async function createReport(
   options: UserOptions,
   _probe: 'XML'
