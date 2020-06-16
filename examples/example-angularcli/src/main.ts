a883b0c2dab29bf9aec646476a94685b7637c3de
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-06-16 21:21:43 +0200

    installed docx-templates to see if it compiles (it doesn't because of require('vm'))

--- examples/example-angularcli/src/main.ts
+++ examples/example-angularcli/src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { createReport } from 'docx-templates';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+//TODO: placeholder to see if it compiles
+createReport({
+  template: new Buffer('abcd'),
+})
