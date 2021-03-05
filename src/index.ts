313284d700b48deae7e1c320057abc07219194eb
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2021-03-05 11:02:50 +0100

    Export error types at the library level.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import createReport, { listCommands, getMetadata } from './main';
-export { createReport, listCommands, getMetadata };
+import createReport from './main';
+export { listCommands, getMetadata } from './main';
+export {
+  NullishCommandResultError,
+  CommandSyntaxError,
+  InvalidCommandError,
+  CommandExecutionError,
+  ImageError,
+  InternalError,
+  TemplateParseError,
+} from './errors';
+export { createReport };
 export default createReport;
