7f0e536afc98b49bd2fff812e38bfc5f8986e440
Sebastian Krüger <2pi_r2@gmx.de>
2021-08-05 10:53:12 +0200

    add some comments to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,16 @@ import esbuild from 'rollup-plugin-esbuild'
 import node from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
+/**
+ * Creates a config from a few params.
+ * 
+ * @param {string} inFile Name of input file
+ * @param {string} outFile Name of output file
+ * @param {string} target Target es type for exampe es2017
+ * @param {boolean} isNode Is Node env
+ * @param {string} format Wich output format umd / es / cjs / amd
+ * @returns 
+ */
 function defineConfig(inFile, outFile, target, isNode, format) {
   return {
     input: './src/' + inFile,
@@ -14,6 +24,7 @@ function defineConfig(inFile, outFile, target, isNode, format) {
         preferBuiltins: false
       }),
       isNode ? null : commonjs(),
+      // Set some node specific globals
       isNode ? null : replace({
         values: {
           'process.env.NODE_DEBUG': false,
@@ -31,6 +42,7 @@ function defineConfig(inFile, outFile, target, isNode, format) {
         target: target,
         minify: true
       }),
+      // Map modules to polyfill
       isNode ? null : {
         name: 'module-map',
         resolveId(id) {
@@ -47,7 +59,10 @@ function defineConfig(inFile, outFile, target, isNode, format) {
 }
 
 export default [
+  // Browser build.
   defineConfig('browser.ts', 'browser.js', 'es2017', false, 'es'),
+  // NodeJS build with require
   defineConfig('index.ts', 'index.js', 'es6', true, 'cjs'),
+  // NodeJS build with es module syntax wich is better for bundles
   defineConfig('index.ts', 'index.es.mjs', 'es6', true, 'es'),
 ]
