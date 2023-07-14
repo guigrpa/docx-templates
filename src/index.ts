2a6aa00d750da5f8a1ba1d11b4be69eae84028ad
Jurriaan Barkey Wolf <info@barkeywolf.consulting>
2023-07-14 15:50:37 +0200

    Issue #314: Ensure all custom error types are exported.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import createReport from './main';
 export { listCommands, getMetadata } from './main';
-export {
-  NullishCommandResultError,
-  CommandSyntaxError,
-  InvalidCommandError,
-  CommandExecutionError,
-  ImageError,
-  InternalError,
-  TemplateParseError,
-  ObjectCommandResultError,
-} from './errors';
+export * from './errors';
 import type { QueryResolver } from './types';
 export { createReport, QueryResolver };
 export default createReport;
